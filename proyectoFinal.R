8b2d453814e10b108ebf9336f9454464407e9e48
farid7 <diraf_thechils@hotmail.com>
2016-12-12 18:46:58 -0600

    implementando funcino de perdida (Kaggel-Walmart)

--- proyectoFinal.R
+++ proyectoFinal.R
@@ -40,7 +40,7 @@ library(snn)
 ############################################################
 ###--Reading data
 ############################################################
-setwd("~/Documents/farid/aprendizajeMaquina/proyectoFinal")
+setwd("/home/farid/Documents/aprendizaje maquina/proyecto final")
 data <- data.frame(read.csv("train.csv"))
 #data <- sample_n(data, size=1000)
 
@@ -63,6 +63,16 @@ data <- select(data, TripType,  Weekday, ScanCount, DepartmentDescription)
 #############---Visualizing labels---############
 ############################################################
 pie(sort(round(table(data$TripType)*100)/length(data$TripType)), labels = names(sort(table(data$TripType))), main="Frequencies of TripType labels")
+
+############################################################
+#############---Preprocessing data---############
+############################################################
+pp_data <- preProcess(data, method=c("scale", "center"),  na.remove=TRUE)
+pp_data
+
+transformed <- data.frame(predict(pp_data, newdata = data))
+transformed <- na.omit(transformed)
+summary(transformed)
 
 ############################################################
 #############---Training & Testing data---############
@@ -99,10 +109,10 @@ registerDoSNOW(cl)
 #############---Make model---############
 ############################################################
 system.time(rfcv <- train(x = select(ttrain, Weekday, ScanCount, DepartmentDescription),
-                         y = ttrain$TripType, 
-                         method = "C5.0",
+                          y = ttrain$TripType, 
+                          method = "C5.0",
                           importance = TRUE,
-                         trControl = control))
+                          trControl = control))
 
 #parRF, C5.0
 
@@ -120,7 +130,7 @@ rfcv
 prueba <- data.frame(read.csv("test.csv"))
 prueba <- select(prueba,  Weekday, ScanCount, DepartmentDescription)
 prediccion <- predict(rfcv, prueba)
-
+prediccion2 <- predict(rfcv, prueba, type="prob")
 ############################################################
 #############---Save values---############
 ############################################################
@@ -128,6 +138,21 @@ prueba <- data.frame(read.csv("test.csv"))
 resultado <- data.frame(TripType=prediccion, prueba)
 write.csv(resultado, "pronosticosWalmart.csv", col.names=TRUE, row.names = FALSE)
 
+############################################################
+#############---Prediction of score in Kaggle---############
+############################################################
+testPred <- predict(rfcv, ttest, type="prob")
+write.csv(testPred, "PrediccionesWalmart.csv", col.names=TRUE, row.names = FALSE)
+
+one_hot_y <- dummies::dummy.data.frame(ttest, names = "TripType") %>% 
+  select(-Weekday, -ScanCount, -DepartmentDescription)
 
 
+loss_function <- function(df_real, df_pred){
+  #(-1/N)(sum(sum(prob(etiqueta))))
+  mult <- df_real * log(df_pred)
+  suma <- mult %>% sum()
+  return(-suma/nrow(df_real))
+}
 
+loss_function(one_hot_y, testPred)
